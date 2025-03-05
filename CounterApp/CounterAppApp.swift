a24bc99de6858de46fb0e647238f0765f806d34e
Merve Bulut <47273479+merve-bulut@users.noreply.github.com>
2025-03-05 23:00:04 +0300

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Merve Bulut on 5.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
